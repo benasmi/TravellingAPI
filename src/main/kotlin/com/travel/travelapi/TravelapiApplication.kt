d1cbbdc3826443afe0451fd74910ace15b531526
pijoneris <benasmiliunas@gmail.com>
2020-04-20 23:28:04 +0300

    initial

---
+++ src/main/kotlin/com/travel/travelapi/TravelapiApplication.kt
@@ -0,0 +1,11 @@
+package com.travel.travelapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TravelapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<TravelapiApplication>(*args)
+}
